68a9daf305c0a6252ae69762d219e457fec742d2
brown55razor <dsutherland@gmail.com>
2012-11-28 10:23:24 -0500

    Added new activities for client and server

---
+++ perimeterWatch/src/main/java/mil/spawar/npe/ServerModeActivity.java
@@ -0,0 +1,56 @@
+package mil.spawar.npe;
+
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+public class ServerModeActivity extends ListActivity{
+	 private static String TAG = "testApp";
+		
+	    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+	    ArrayList<String> listItems=new ArrayList<String>();
+
+	    //DEFINING STRING ADAPTER WHICH WILL HANDLE DATA OF LISTVIEW
+	    ArrayAdapter<String> adapter;
+
+	    //RECORDING HOW MUCH TIMES BUTTON WAS CLICKED
+	    int clickCounter=0;
+
+	    @Override
+	    public void onCreate(Bundle icicle) {
+	        super.onCreate(icicle);
+			Log.i(TAG, "onCreate");
+			
+	        setContentView(R.layout.serverview);
+	        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listItems);
+	        setListAdapter(adapter);
+	        
+	        Button quit=(Button) findViewById(R.id.quit);
+	        
+	        quit.setOnClickListener(new View.OnClickListener(){
+				@Override
+				public void onClick(View arg0) {
+					finish();
+		            System.exit(0);
+				}
+	        	
+	        });
+	        
+	        listItems.add("Client 1");
+	        listItems.add("Client 2");
+	        listItems.add("Client 3");
+	    }
+
+	    
+	    
+	    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+	    public void addItems(View v) {
+	        listItems.add("Clicked : "+clickCounter++);
+	        adapter.notifyDataSetChanged();
+	    }
+	}
